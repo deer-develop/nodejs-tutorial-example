8b8521bda0a011690d81f0558a27d93431d0b247
hurihuchi <gmldnr96@gmail.com>
2022-03-20 16:47:07 +0900

    feat: 프로그램 실행, 입출력 처리를 담당하는 Program클래스 생성

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as readline from "readline";
+import { Program } from "./Program";
+import { Menu } from "./Menu";
+
 export const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const program = new Program([new Menu(1, "목록 조회"), new Menu(2, "쓰기")]);
+program.run();
